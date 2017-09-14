9a64c2ddb3ad00ae4cb2e86339b7553d06b2ed21
Viswanath Pasumarthi <pasumart@buffalo.edu>
2017-09-14 13:57:26 -0400

    removed redundant parameters; tidied up

--- modelscripts/analysisscripts/uniquePathways/inputPathways.py
+++ modelscripts/analysisscripts/uniquePathways/inputPathways.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python
 
 from generateUniquePathways import generateUniquePathways
-import numpy as np
 import os
 
 # Frequently modified input parameters:
-cutoffDistKey = 'O:O'
-cutoff = 4
+cutoffDistKey = 'Zr:Zr'
+cutoff = 6
 base = 0.05
 prec = 2
-nDim = 3
-cwd = os.path.dirname(os.path.realpath(__file__))
+inputCoordinateFileName = 'CONTCAR'
 
-inputCoordinateFileName = 'POSCAR'
+cwd = os.path.dirname(os.path.realpath(__file__))
 inputFileLocation = os.path.join(cwd, inputCoordinateFileName)
 
 generateUniquePathways(inputFileLocation, cutoffDistKey, cutoff, base, prec, cwd)
