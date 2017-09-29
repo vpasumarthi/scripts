b3f31cff62d4ec49348c3b2da1b871a4ed6fc4a4
Viswanath Pasumarthi <pasumart@buffalo.edu>
2017-09-29 16:56:57 -0400

    modified to PEP8 compliant

--- modelscripts/analysisscripts/uniquePathways/inputPathways.py
+++ modelscripts/analysisscripts/uniquePathways/inputPathways.py
@@ -7,24 +7,36 @@ import os
 # Frequently modified input parameters:
 inputCoordinateFileName = 'POSCAR'
 cutoffDistKey = 'O:O'
-avoidElementType = 'O' # 'O' for cutoffDistKey = 'O:O'; '' for V
+avoidElementType = 'O'  # 'O' for cutoffDistKey = 'O:O'; '' for V
 neighborCutoff = 4
-bridgeCutoff = 2.51 # 2.62 for Zr:Zr; 3.50 for S:S; 2.51 for O:O
-roundLatticeParameters = {} #{'base': 0.05, 'prec': 2}
+bridgeCutoff = 2.51  # 2.62 for Zr:Zr; 3.50 for S:S; 2.51 for O:O
+roundLatticeParameters = {}  # {'base': 0.05, 'prec': 2}
 printPathwayList = 1
 printEquivalency = 0
 equivalencyPrec = 4
 pathwayPrec = 5
 
-# unitCellCenterSiteClassList = unitCellNeighborSiteClassList = np.array([1, 1, 2, 2, 1, 1, 1, 1, 2, 2, 1, 1]) # 'S:S'
-unitCellCenterSiteClassList = unitCellNeighborSiteClassList = np.array([1, 1, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2, 2, 2, 1, 1]) # 'O:O'
-classList = [unitCellCenterSiteClassList, unitCellNeighborSiteClassList]
+# 'S:S': np.array([1, 1, 2, 2, 1, 1, 1, 1, 2, 2, 1, 1])
+# 'O:O': np.array([1, 1, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2, 2, 2, 1, 1])
+unitCellCenterSiteClassList = unitCellNeighborSiteClassList = \
+                    np.array([1, 1, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2, 2, 2, 1, 1])
+# 'V:V':
 # classList = []
+# 'O:O' or 'S:S':
+# classList = [unitCellCenterSiteClassList, unitCellNeighborSiteClassList]
+classList = [unitCellCenterSiteClassList, unitCellNeighborSiteClassList]
 
-desiredCoordinateParameters = {} # {'desiredSystemSize': np.array([2, 2, 2]), 'distList': [2.96781, 2.99576], 'prec': 5}
+# desiredCoordinateParameters = {'desiredSystemSize': np.array([2, 2, 2]),
+#                                'distList': [2.96781, 2.99576],
+#                                'prec': 5}
+# desiredCoordinateParameters = {}
+desiredCoordinateParameters = {}
 
 cwd = os.path.dirname(os.path.realpath(__file__))
 inputFileLocation = os.path.join(cwd, inputCoordinateFileName)
 
-generateUniquePathways(inputFileLocation, cutoffDistKey, neighborCutoff, bridgeCutoff, cwd, pathwayPrec, equivalencyPrec, classList, 
-                       avoidElementType, roundLatticeParameters, printPathwayList, printEquivalency, desiredCoordinateParameters)
+generateUniquePathways(inputFileLocation, cutoffDistKey, neighborCutoff,
+                       bridgeCutoff, cwd, pathwayPrec, equivalencyPrec,
+                       classList, avoidElementType, roundLatticeParameters,
+                       printPathwayList, printEquivalency,
+                       desiredCoordinateParameters)
