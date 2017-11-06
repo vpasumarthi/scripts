bad2c1b3222fa34b5801ae1f1d0cae309067e975
Viswanath <pasumart@buffalo.edu>
2017-11-06 15:39:16 -0500

    avoid all hardcoding

--- modelscripts/ellipsoid/generateMVEE.m
+++ modelscripts/ellipsoid/generateMVEE.m
@@ -1,17 +1,31 @@
-close all
+% Frequently modified parameters
 materialName = 'BVO';
 speciesType = 'hole';
 numSpecies = 2;
-inputFileName = 'unwrappedTraj.dat';
 numTrajRecorded = 1.00E+02;
 tFinal = 1.00E-05;
 timeInterval = 1.00E-09;
+maxLim = 1000; % Hem-2e: 1400, BVO-2e: 700, BVO-2h: 1000
+xlimits = [-maxLim, maxLim];
+ylimits = [-maxLim, maxLim];
+zlimits = [-maxLim, maxLim];
+numFrames = 100;
+
+% Not so frequently modified parameters
+inputFileName = 'unwrappedTraj.dat';
 bohr2ang = 0.529177249;
 positionArray = dlmread('unwrappedTraj.dat') * bohr2ang;
 numPathStepsPerTraj = round(tFinal / timeInterval) + 1;
 positionArraySize = size(positionArray);
 nSpecies = positionArraySize(2) / 3;
 dataArray = zeros(numPathStepsPerTraj, numTrajRecorded * nSpecies, 3);
+numStepsPerFrame = round((numPathStepsPerTraj - 1) / numFrames);
+tol = 0.001;
+if numSpecies > 1
+    speciesTail = 's';
+else
+    speciesTail = '';
+end
 
 for trajIndex = 0:numTrajRecorded-1
     headStart = trajIndex * numPathStepsPerTraj;
@@ -25,13 +39,7 @@ for trajIndex = 0:numTrajRecorded-1
     end
 end
 
-loops = 1000;
-F(loops) = struct('cdata',[],'colormap',[]);
-if numSpecies > 1
-    speciesTail = 's';
-else
-    speciesTail = '';
-end
+F(numFrames) = struct('cdata',[],'colormap',[]);
 outputFileName = strcat(materialName, '_', num2str(numSpecies), ...
                         speciesType, speciesTail, '.avi');
 v = VideoWriter(outputFileName);
@@ -42,11 +50,10 @@ xlabelstr = sprintf('x (%c)', 197);
 ylabelstr = sprintf('y (%c)', 197);
 zlabelstr = sprintf('z (%c)', 197);
 for step = 0:numPathStepsPerTraj-1
-    if mod(step, 100) == 0 && step ~= 0
+    if mod(step, numStepsPerFrame) == 0 && step ~= 0
         Pext = dataArray(step + 1, :, :);
-        P = reshape(Pext, 200, 3)';
-        t = 0.001;
-        [A , C] = MinVolEllipse(P, t);
+        P = reshape(Pext, numTrajRecorded * nSpecies, 3)';
+        [A , C] = MinVolEllipse(P, tol);
         plot3(P(1,:),P(2,:),P(3,:),'*')
         hold on
         Ellipse_plot(A,C)
@@ -56,17 +63,12 @@ for step = 0:numPathStepsPerTraj-1
         zlabel(zlabelstr)
         figTitle = ['Diffusion of ', num2str(numSpecies), ' ', ...
                     speciesType, speciesTail, ...
-                    ' depicted over 100 traj in ms-BVO'];
+                    ' depicted over ', num2str(numTrajRecorded), ...
+                    ' traj in ms-BVO'];
         title(figTitle)
-        xlim([-1000, 1000]) % bvo_2hole
-        ylim([-1000, 1000]) % bvo_2hole
-        zlim([-1000, 1000]) % bvo_2hole
-%         xlim([-700, 700]) % bvo_2elec
-%         ylim([-700, 700]) % bvo_2elec
-%         zlim([-700, 700]) % bvo_2elec
-%         xlim([-1400, 1400]) % hem_2elec
-%         ylim([-1400, 1400]) % hem_2elec
-%         zlim([-1400, 1400]) % hem_2elec
+        xlim(xlimits)
+        ylim(ylimits)
+        zlim(zlimits)
         F(index) = getframe(gcf);
         writeVideo(v,F(index));
         index = index + 1;
