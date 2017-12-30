2d9ae22e8fe8818ec0247cc87f43f55544670189
'Viswanath <'pasumart@buffalo.edu'>
2017-12-30 17:24:12 -0500

    got rid off bridgeSearchDepth

--- vaspscripts/cluster/generateCluster.py
+++ vaspscripts/cluster/generateCluster.py
@@ -6,15 +6,14 @@ import os
 # Frequently modified input parameters:
 inputCoordinateFileName = 'CONTCAR'
 siteElementTypeList = ['V', 'V']
-siteNumberList = [14, 32]
-bridgeSearchDepth = 2
+siteNumberList = [38, 89]
+bondLimits = {'Bi:O': 2.60, 'V:O': 1.80}
 terminatingElementType = 'H'
-bondLimits = {'Bi:O': 2.6608, 'V:O': 2.84939}
 terminatingBondDistance = 0.96  # angstrom
 
 cwd = os.path.dirname(os.path.realpath(__file__))
 inputFileLocation = os.path.join(cwd, inputCoordinateFileName)
 
 cluster(inputFileLocation, siteElementTypeList,
-        siteNumberList, bondLimits, bridgeSearchDepth, terminatingElementType,
+        siteNumberList, bondLimits, terminatingElementType,
         terminatingBondDistance)
