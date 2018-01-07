7b7afaa06a0034be9b874781a8aaaf840d4c8146
'Viswanath <'pasumart@buffalo.edu'>
2018-01-07 16:17:25 -0500

    minor change: add dst_file_name

--- vaspscripts/cluster/generate_hnd_input.py
+++ vaspscripts/cluster/generate_hnd_input.py
@@ -2,7 +2,7 @@
 
 from vaspscripts.cluster.hnd_input import hnd_input
 
-input_coordinate_file_name = 'POSCAR01.cart'
-dst_file_name = 'POSCAR01.hndin'
-dst_element_types = ['V', 'Bi', 'O', 'H']
+input_coordinate_file_name = 'POSCAR01'
+dst_file_name = input_coordinate_file_name + '.hndin'
+dst_element_types = ['V', 'O', 'H']
 hnd_input(input_coordinate_file_name, dst_file_name, dst_element_types)
