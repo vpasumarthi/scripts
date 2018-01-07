bd80140d32254fc3954b7a19bbd73c4baabcc12e
'Viswanath <'pasumart@buffalo.edu'>
2018-01-07 16:17:03 -0500

    minor change: add outputCoordinateFileName

--- vaspscripts/cluster/generateCluster.py
+++ vaspscripts/cluster/generateCluster.py
@@ -5,15 +5,19 @@ import os
 
 # Frequently modified input parameters:
 inputCoordinateFileName = 'CONTCAR'
-siteIndexList = [1134, 1144]  # site number - 1
+outputCoordinateFileName = 'POSCAR'
+siteIndexList = [1134, 1037]  # site number - 1
 bondLimits = {'Bi:O': 2.60, 'V:O': 1.80}
 terminatingElementType = 'H'
 terminatingBondDistance = 0.96  # angstrom
 oxidationList = {'Bi': +3, 'V': +5, 'O': -2, 'H': +1}
+bridgeSearchDepth = 1
+chargeNeutral = 1
 prec = 3
 
 cwd = os.path.dirname(os.path.realpath(__file__))
 inputFileLocation = os.path.join(cwd, inputCoordinateFileName)
 
-cluster(inputFileLocation, siteIndexList, bondLimits, terminatingElementType,
-        terminatingBondDistance, oxidationList, prec)
+cluster(inputFileLocation, outputCoordinateFileName, siteIndexList, bondLimits,
+        terminatingElementType, terminatingBondDistance, oxidationList,
+        bridgeSearchDepth, chargeNeutral, prec)
