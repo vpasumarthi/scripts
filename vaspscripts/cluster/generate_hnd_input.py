dfeaf1919d362ef2e5384f846bcb1593f6183fef
'Viswanath <'pasumart@buffalo.edu'>
2018-01-12 15:23:09 -0500

    remove user scripts

--- vaspscripts/cluster/generate_hnd_input.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-from vaspscripts.cluster.hnd_input import hnd_input
-
-input_coordinate_file_name = 'POSCAR01'
-dst_file_name = input_coordinate_file_name + '.hndin'
-dst_element_types = ['V', 'O', 'H']
-hnd_input(input_coordinate_file_name, dst_file_name, dst_element_types)
