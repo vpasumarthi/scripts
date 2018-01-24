715866b8fb0be1d627d126b31cf0862711319a0d
'Viswanath <'pasumart@buffalo.edu'>
2018-01-23 19:08:37 -0500

    restructure hnd_input module; add frz_orbitals
    split hnd_input function to basis_order, geometry
    add frz_orbitals to hnd_input

--- vaspscripts/cluster/hnd_input.py
+++ vaspscripts/cluster/hnd_input.py
@@ -5,7 +5,7 @@ import numpy as np
 from PyCT import io
 
 
-def hnd_input(input_coordinate_file_name, dst_file_name, dst_element_types):
+def basis_order(input_coordinate_file_name, dst_file_name, dst_element_types):
     poscar_info = io.read_poscar(input_coordinate_file_name)
     coordinate_type = poscar_info[-2]
     if coordinate_type == 'Cartesian':
@@ -37,6 +37,22 @@ def hnd_input(input_coordinate_file_name, dst_file_name, dst_element_types):
             line = (''.join([' ' * headspace, element_list, ',\n']))
             dst_file.write(line)
     dst_file.write(' $END\n\n')
+    return None
+
+
+def geometry(input_coordinate_file_name, dst_file_name, dst_element_types):
+    poscar_info = io.read_poscar(input_coordinate_file_name)
+    coordinate_type = poscar_info[-2]
+    if coordinate_type == 'Cartesian':
+        cartesian_coordinates = poscar_info[-1]
+    else:
+        lattice_matrix = poscar_info[0]
+        fractional_coordinates = poscar_info[-1]
+        cartesian_coordinates = np.dot(fractional_coordinates, lattice_matrix)
+    element_types = poscar_info[1]
+    n_elements = poscar_info[2]
+    open(dst_file_name, 'w').close()
+    dst_file = open(dst_file_name, 'a')
 
     # data group to specify geometry
     column_sep_width = 3
@@ -59,3 +75,37 @@ def hnd_input(input_coordinate_file_name, dst_file_name, dst_element_types):
             dst_file.write(line)
     dst_file.write('\n $END\n')
     dst_file.close()
+    return None
+
+
+def frz_orbitals(orbital_indices, dst_file_name):
+    open(dst_file_name, 'w').close()
+    dst_file = open(dst_file_name, 'a')
+
+    headspace = 15
+    num_orbitals_per_line = 10
+    start_line_parameter = 'IFRZ='
+    len_parameter = len(start_line_parameter)
+    start_line_headspace = headspace - len_parameter
+    orbital_index_width = len(str(max(orbital_indices)))
+    num_orbitals = num_unprinted_orbitals = len(orbital_indices)
+    start_index = 0
+    while num_unprinted_orbitals > 0:
+        if num_unprinted_orbitals > num_orbitals_per_line:
+            end_index = start_index + num_orbitals_per_line
+            num_unprinted_orbitals -= num_orbitals_per_line
+        else:
+            end_index = num_orbitals - 1
+            num_unprinted_orbitals = 0
+        orbital_index_list = ','.join(
+                            [f'{orbital_indices[index]:>{orbital_index_width}}'
+                             for index in range(start_index, end_index)])
+        if start_index == 0:
+            line = (''.join([' ' * start_line_headspace, start_line_parameter,
+                             orbital_index_list, ',\n']))
+        else:
+            line = (''.join([' ' * headspace, orbital_index_list, ',\n']))
+        start_index = end_index
+        dst_file.write(line)
+    dst_file.close()
+    return None
