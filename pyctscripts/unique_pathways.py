cdb7e71f998d7e37f611496f389bec8c49d42f1c
'Viswanath <'pasumart@buffalo.edu'>
2018-02-22 15:53:33 -0500

    use PyCT.io.read_poscar

--- pyctscripts/unique_pathways.py
+++ pyctscripts/unique_pathways.py
@@ -4,34 +4,7 @@ import os
 
 import numpy as np
 
-
-def read_poscar(input_file_path):
-    lattice_matrix = np.zeros((3, 3))
-    lattice_parameter_index = 0
-    lattice_parameters_line_range = range(3, 6)
-    input_file = open(input_file_path, 'r')
-    for line_index, line in enumerate(input_file):
-        line_number = line_index + 1
-        if line_number in lattice_parameters_line_range:
-            lattice_matrix[lattice_parameter_index, :] = np.fromstring(line,
-                                                                    sep=' ')
-            lattice_parameter_index += 1
-        elif line_number == 6:
-            element_types = line.split()
-        elif line_number == 7:
-            n_elements_per_unit_cell = np.fromstring(line, dtype=int, sep=' ')
-            total_elements_per_unit_cell = n_elements_per_unit_cell.sum()
-            fractional_unit_cell_coords = np.zeros((
-                                            total_elements_per_unit_cell, 3))
-            element_index = 0
-        elif line_number > 8 and element_index < total_elements_per_unit_cell:
-            fractional_unit_cell_coords[element_index, :] = np.fromstring(line,
-                                                                      sep=' ')
-            element_index += 1
-    input_file.close()
-    output = np.array([lattice_matrix, element_types, n_elements_per_unit_cell,
-                       fractional_unit_cell_coords], dtype=object)
-    return output
+from PyCT.io import read_poscar
 
 
 def generate_quantum_indices(system_size, system_element_index,
@@ -63,8 +36,19 @@ def unique_pathways(input_file_location, cutoff_dist_key, neighbor_cutoff,
                    print_equivalency=0, desired_coordinate_parameters={}):
     """ generate unique pathways for the given set of element types"""
     # define input parameters
-    [lattice_matrix, element_types, n_elements_per_unit_cell,
-     fractional_unit_cell_coords] = read_poscar(input_file_location)
+    poscar_info = read_poscar(input_file_location)
+    lattice_matrix = poscar_info['lattice_matrix']
+    element_types = poscar_info['element_types']
+    n_elements_per_unit_cell = poscar_info['num_elements']
+    coordinate_type = poscar_info['coordinate_type']
+    unit_cell_coords = poscar_info['coordinates']
+
+    if coordinate_type == 'Direct':
+        fractional_unit_cell_coords = unit_cell_coords
+    elif coordinate_type == 'Cartesian':
+        fractional_unit_cell_coords = np.dot(
+                    unit_cell_coords, np.linalg.inv(lattice_matrix))
+
     n_element_types = len(element_types)
     total_elements_per_unit_cell = n_elements_per_unit_cell.sum()
     element_type_index_list = np.repeat(np.arange(n_element_types),
