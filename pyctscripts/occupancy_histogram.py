7461d9b978fbb2a3ac22c84d25aa3cae571819f3
'Viswanath <'pasumart@buffalo.edu'>
2018-05-15 15:37:43 -0400

    eliminate num_probe_shells as input. replaced as self.num_shells.

--- pyctscripts/occupancy_histogram.py
+++ pyctscripts/occupancy_histogram.py
@@ -7,10 +7,9 @@ import matplotlib.pyplot as plt
 class Occupancy(object):
     """Class definition to generate occupancy histogram files"""
 
-    def __init__(self, num_probe_shells, color_list, dst_path,
-                 shell_index_file_path, occupancy_file_path):
+    def __init__(self, color_list, dst_path, shell_index_file_path,
+                 occupancy_file_path):
         # Load occupancy parameters
-        self.num_probe_shells = num_probe_shells
         self.color_list = color_list
         self.dst_path = dst_path
         self.shell_index_file_path = shell_index_file_path
@@ -20,13 +19,14 @@ class Occupancy(object):
             for line in shell_index_file:
                 self.shell_indices_list.append(
                     [int(index) for index in line.split('\n')[0].split(', ')])
+        self.num_shells = len(self.shell_indices_list) - 1
         self.probe_indices = []
         self.site_population_list = []
-        for shell_index in range(num_probe_shells+2):
-            if shell_index == num_probe_shells + 1:
+        for shell_index in range(self.num_shells+1):
+            if shell_index == self.num_shells + 1:
                 self.probe_indices.append(sorted(
                     [item
-                     for sublist in self.shell_indices_list[num_probe_shells+1:]
+                     for sublist in self.shell_indices_list[self.num_shells+1:]
                      for item in sublist]))
             else:
                 self.probe_indices.append(sorted(
@@ -37,7 +37,7 @@ class Occupancy(object):
         with self.occupancy_file_path.open('r') as occupancy_file:
             for line in occupancy_file:
                 site_index = int(line.split('\n')[0])
-                for shell_index in range(num_probe_shells+2):
+                for shell_index in range(self.num_shells+1):
                     if site_index in self.probe_indices[shell_index]:
                         list_index = self.probe_indices[shell_index].index(site_index)
                         self.site_population_list[shell_index][list_index] += 1
@@ -79,16 +79,16 @@ class Occupancy(object):
         fig = plt.figure()
         plt.title('Shell-wise occupancy')
         ax = fig.add_subplot(111)
-        for shell_index in range(self.num_probe_shells+2):
+        for shell_index in range(self.num_shells+1):
             mean_value = int(np.mean(self.site_population_list[shell_index]))
             ax.bar(shell_index, mean_value, color=self.color_list[shell_index])
             ax.text(shell_index, 1.01 * mean_value, str(mean_value),
                     color='black', horizontalalignment='center')
         ax.set_xlabel(f'Shell Number')
         ax.set_ylabel('Average shell occupancy')
-        xticks_list = [str(index) for index in range(self.num_probe_shells+2)]
+        xticks_list = [str(index) for index in range(self.num_shells+1)]
         xticks_list[-1] += '+'
-        plt.xticks(range(self.num_probe_shells+2), xticks_list)
+        plt.xticks(range(self.num_shells+1), xticks_list)
         figure_name = 'shell-wise_occupancy.png'
         figure_path = self.dst_path / figure_name
         plt.tight_layout()
