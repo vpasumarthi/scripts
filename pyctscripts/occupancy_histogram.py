e0f8bae743c5291192cb4b1ac211019788835a07
'Viswanath <'pasumart@buffalo.edu'>
2018-05-15 16:52:34 -0400

    read new-format of site_indices

--- pyctscripts/occupancy_histogram.py
+++ pyctscripts/occupancy_histogram.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import re
+
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
@@ -8,31 +10,37 @@ import matplotlib.gridspec as gridspec
 class Occupancy(object):
     """Class definition to generate occupancy histogram files"""
 
-    def __init__(self, color_list, dst_path, shell_index_file_path,
-                 occupancy_file_path):
+    def __init__(self, site_indices_file_path, occupancy_file_path,
+                 dst_path, color_list):
         # Load occupancy parameters
         self.color_list = color_list
         self.num_colors = len(self.color_list)
         self.dst_path = dst_path
-        self.shell_index_file_path = shell_index_file_path
+        self.site_indices_file_path = site_indices_file_path
         self.occupancy_file_path = occupancy_file_path
-        self.shell_indices_list = []
-        with self.shell_index_file_path.open('r') as shell_index_file:
-            for line in shell_index_file:
-                self.shell_indices_list.append(
-                    [int(index) for index in line.split('\n')[0].split(', ')])
-        self.num_shells = len(self.shell_indices_list) - 1
+        self.shell_indices_dict = {}
+        with self.site_indices_file_path.open('r') as site_indices_file:
+            for line in site_indices_file:
+                split_elements = re.split(',|\n', line)
+                shell_index = int(split_elements[3])
+                site_index = int(split_elements[0])
+                if shell_index in self.shell_indices_dict:
+                    self.shell_indices_dict[shell_index].append(site_index)
+                else:
+                    self.shell_indices_dict[shell_index] = [site_index]
+                
+        self.num_shells = len(self.shell_indices_dict) - 1
         self.probe_indices = []
         self.site_population_list = []
         for shell_index in range(self.num_shells+1):
             if shell_index == self.num_shells + 1:
                 self.probe_indices.append(sorted(
                     [item
-                     for sublist in self.shell_indices_list[self.num_shells+1:]
+                     for sublist in self.shell_indices_dict[self.num_shells+1:]
                      for item in sublist]))
             else:
                 self.probe_indices.append(sorted(
-                    [item for item in self.shell_indices_list[shell_index]]))
+                    [item for item in self.shell_indices_dict[shell_index]]))
             self.site_population_list.append(
                                     [0] * len(self.probe_indices[shell_index]))
 
