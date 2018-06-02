adf98eddeffcbb779e894391112f6930d8c22ae2
'Viswanath <'pasumart@buffalo.edu'>
2018-06-02 15:50:59 -0400

    define generate_res_time_distribution function.

--- pyctscripts/occupancy_histogram.py
+++ pyctscripts/occupancy_histogram.py
@@ -3,6 +3,7 @@
 import re
 
 import numpy as np
+from scipy.stats import linregress
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 
@@ -164,4 +165,82 @@ class Occupancy(object):
         figure_path = self.src_path / figure_name
         plt.tight_layout()
         plt.savefig(str(figure_path))
+        return None
+
+    def generate_res_time_distribution(self, res_time_pool, n_traj):
+        plt.switch_backend('Agg')
+        fig = plt.figure()
+        plt.title('Residence length distribution in the doping region')
+        ax1 = fig.add_subplot(111)
+        cumulative_res_time_pool = [res_time
+                                    for traj_res_time_pool in res_time_pool
+                                    for res_time in traj_res_time_pool]
+        min_res_time = min(cumulative_res_time_pool)
+        max_res_time = max(cumulative_res_time_pool)
+        num_bins = max_res_time - min_res_time + 1
+        bin_edges = np.arange(min_res_time-1, max_res_time+1) + 0.5
+        cumulative_pop, _ = np.histogram(cumulative_res_time_pool, bin_edges)
+        bin_centers = 0.5 * (bin_edges[1:] + bin_edges[:-1])
+        traj_freq = []
+        mean_residence_length_pool = []
+        for traj_res_time_pool in res_time_pool:
+            i_traj_freq, _ = np.histogram(traj_res_time_pool, bin_edges)
+            i_total_freq = sum(i_traj_freq)
+            i_prob_freq = i_traj_freq / i_total_freq
+            i_mean_residence_length = np.inner(bin_centers, i_prob_freq)
+            mean_residence_length_pool.append(i_mean_residence_length)
+            traj_freq.append(i_traj_freq)
+        traj_freq = np.asarray(traj_freq)
+        mean_freq = np.mean(traj_freq, axis=0)
+        std_pop = np.std(traj_freq, axis=0)
+        plt.plot(bin_centers, mean_freq, 'o-', color='black',
+                mfc=self.color_list[1], mec='black')
+        plt.errorbar(bin_centers, mean_freq, yerr=std_pop, fmt='o', capsize=3,
+                    color='black', mfc='none', mec='none')
+
+        plt.yscale('linear')
+        xmin, xmax = plt.xlim()
+        ymin, ymax = plt.ylim()
+        mean_residence_length = np.mean(mean_residence_length_pool)
+        std_mean_residence_length = np.std(mean_residence_length_pool)
+        plt.plot([mean_residence_length] * 2, [ymin, ymax], '--',
+                 color=self.color_list[2])
+        plt.ylim([ymin, ymax])
+        floor_index = np.where(bin_centers==np.floor(mean_residence_length))[0]
+        err_y_pos = (mean_freq[0] + mean_freq[floor_index]) / 4
+        ax1.errorbar(mean_residence_length, err_y_pos,
+                    xerr=std_mean_residence_length, fmt='o', capsize=3,
+                    color='black', markerfacecolor='none',
+                    markeredgecolor='none')
+        ax1.text(mean_residence_length + std_mean_residence_length + 0.5,
+                0.98 * err_y_pos,
+                f'Mean residence length ~ {mean_residence_length:.3f} steps',
+                verticalalignment='center')
+
+        ax2 = fig.add_axes([0.5, 0.5, 0.35, 0.35], yscale='log')
+        limited_indices = np.where(mean_freq > 1)[0]
+        ax2.plot(bin_centers[limited_indices], mean_freq[limited_indices],
+                 'o-', color='black', mfc=self.color_list[1], mec='black')
+        slope, intercept, r_value, _, _ = \
+            linregress(bin_centers[limited_indices], np.log10(mean_freq[limited_indices]))
+        ax2.plot(bin_centers[limited_indices],
+                 10**(intercept + slope * bin_centers[limited_indices]), '--',
+                 color=self.color_list[2])
+        sign = '-' if slope < 0 else '+'
+        ax2.text(mean_residence_length + std_mean_residence_length + 2,
+                 1.2*err_y_pos,
+                 'log$_{10}$(y) = ' + f'{intercept:.3f} {sign} {abs(slope):.3f}x',
+                 verticalalignment='center')
+        ax2.text(mean_residence_length + 12, 0.4*err_y_pos,
+                 'r$^2$ = ' + f'{r_value**2:.3f}', verticalalignment='center')
+
+        ax1.set_xlabel('Residence length in number of kmc steps')
+        ax1.set_ylabel('Frequency')
+        figure_name = f'Residence length distribution_{n_traj}traj.png'
+        figure_path = self.src_path / figure_name
+        plt.savefig(str(figure_path))
+        res_time_data = np.hstack((bin_centers[:, None], mean_freq[:, None],
+                                   std_pop[:, None]))
+        res_time_data_file_name = f'Residence length distribution_{n_traj}traj.dat'
+        np.savetxt(res_time_data_file_name, res_time_data)
         return None
