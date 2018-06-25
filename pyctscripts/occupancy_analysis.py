41b83e46fc36331c490eb66e0e14139073be5a6b
'Viswanath <'pasumart@buffalo.edu'>
2018-06-25 18:12:26 -0400

    use step_limits of array data type

--- pyctscripts/occupancy_analysis.py
+++ pyctscripts/occupancy_analysis.py
@@ -282,6 +282,7 @@ class Occupancy(object):
             step_system_size_master_list.append(step_system_size)
             step_limits.append(old_index + step_system_size[ld])
             old_index += step_system_size[ld]
+        step_limits = np.asarray(step_limits)
         for traj_number in range(1, n_traj+1):
             occupancy_data = self.read_occupancy_data(traj_number)
             for site_index in occupancy_data:
