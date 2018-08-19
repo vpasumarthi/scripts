d45e385d4c2b4449ab4dd6d246bea73840852fba
Viswanath Pasumarthi <pasumart@buffalo.edu>
2018-08-19 12:45:18 -0400

    copy reference input files to working directories of each site index

--- vaspscripts/input_workflow.py
+++ vaspscripts/input_workflow.py
@@ -3,6 +3,7 @@
 # File to generate input files for charge localization jobs
 
 from pathlib import Path
+from shutil import copy
 
 import numpy as np
 
@@ -10,8 +11,13 @@ import numpy as np
 site_indices = np.loadtxt('site_indices.dat', dtype=int)
 cwd = Path.cwd()
 partial_run_index = 1
+copy_file_names = ["POSCAR", "POTCAR", "INCAR", "KPOINTS", "slurmscript", "generate_bond_distortion.py"]
 
 for site_index in site_indices:
+    # Generate input files
     site_dir_path = cwd / f'W36_ox6_V{site_index:02}_ox4'
     work_dir_path = site_dir_path / f'partial_run{partial_run_index:02}'
     Path.mkdir(work_dir_path, parents=True, exist_ok=True)
+
+    for file_name in copy_file_names:
+        copy(cwd / file_name, work_dir_path)
