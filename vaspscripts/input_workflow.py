c01213402d9ac89b3797821ed4cc31d709eb15d0
Viswanath Pasumarthi <pasumart@buffalo.edu>
2018-08-19 13:32:47 -0400

    copy element header from reference POSCAR

--- vaspscripts/input_workflow.py
+++ vaspscripts/input_workflow.py
@@ -15,6 +15,8 @@ partial_run_index = 1
 copy_file_names = ["POSCAR", "POTCAR", "INCAR", "KPOINTS", "slurmscript", "generate_bond_distortion.py"]
 search_term = 'localized_site_number = '
 exp_num_diff_lines = 4
+ref_site_index = 20
+ref_partial_run_index = 1
 
 for site_index in site_indices:
     # Generate input files
@@ -45,6 +47,18 @@ for site_index in site_indices:
                 num_diff_lines += 1
     if num_diff_lines == exp_num_diff_lines:
         move("POSCAR.out", "POSCAR")
+        ref_site_dir_path = cwd / f'W36_ox6_V{ref_site_index:02}_ox4'
+        ref_work_dir_path = ref_site_dir_path / f'partial_run{ref_partial_run_index:02}'
+        ref_coord_file_path = ref_work_dir_path / "POSCAR"
+        old_coord_file_path = work_dir_path / "POSCAR"
+        new_coord_file_path = work_dir_path / "POSCAR.new"
+        with open(ref_coord_file_path) as ref_coords, open(old_coord_file_path) as old_coords, open(new_coord_file_path, 'w') as new_coords:
+            for line_index, (line1, line2) in enumerate(zip(ref_coords, old_coords)):
+                if line_index == 5 or line_index == 6:
+                    new_coords.write(line1)
+                else:
+                    new_coords.write(line2)
+        move(new_coord_file_path, old_coord_file_path)
     else:
         print(f'Unexpected bond distortion output in {work_dir_path}')
     chdir(cwd)
