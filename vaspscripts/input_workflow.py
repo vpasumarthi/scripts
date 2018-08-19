6e365e1217d13758e545beed43ea7d657f3ac834
Viswanath Pasumarthi <pasumart@buffalo.edu>
2018-08-19 13:08:12 -0400

    change site index value in generate_bond_distortion.py

--- vaspscripts/input_workflow.py
+++ vaspscripts/input_workflow.py
@@ -3,7 +3,7 @@
 # File to generate input files for charge localization jobs
 
 from pathlib import Path
-from shutil import copy
+from shutil import copy, copymode, move
 
 import numpy as np
 
@@ -12,6 +12,7 @@ site_indices = np.loadtxt('site_indices.dat', dtype=int)
 cwd = Path.cwd()
 partial_run_index = 1
 copy_file_names = ["POSCAR", "POTCAR", "INCAR", "KPOINTS", "slurmscript", "generate_bond_distortion.py"]
+search_term = 'localized_site_number = '
 
 for site_index in site_indices:
     # Generate input files
@@ -21,3 +22,13 @@ for site_index in site_indices:
 
     for file_name in copy_file_names:
         copy(cwd / file_name, work_dir_path)
+    old_file_path = work_dir_path / 'generate_bond_distortion.py'
+    new_file_path = work_dir_path / 'generate_bond_distortion.py.new'
+    with open(old_file_path) as old_file, open(new_file_path, 'w') as new_file:
+        for line in old_file:
+            if search_term in line:
+                new_file.write(f'{search_term}{site_index}\n')
+            else:
+                new_file.write(line)
+    copymode(old_file_path, new_file_path)
+    move(new_file_path, old_file_path)
