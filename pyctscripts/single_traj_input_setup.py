c2f0e35ba78e50d893f9bc0fb0944259b1db54df
Viswanath Pasumarthi <pasumart@buffalo.edu>
2018-09-15 16:58:55 -0400

    create directories for trajectories in the given range

--- pyctscripts/single_traj_input_setup.py
+++ pyctscripts/single_traj_input_setup.py
@@ -2,4 +2,14 @@
 
 # File to setup input files to perform single trajectory simulations
 
+from pathlib import Path
+
+
+start_traj = 1
+end_traj = 100
+cwd = Path.cwd()
+
+for traj_number in range(start_traj, end_traj+1):
+    traj_dir_path = cwd / f'{traj_number:03}'
+    Path.mkdir(traj_dir_path, parents=True, exist_ok=True)
 
