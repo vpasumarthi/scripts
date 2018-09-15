e3404a140a48131312cfeb9366ca86cd0e87f21c
Viswanath Pasumarthi <pasumart@buffalo.edu>
2018-09-15 17:08:20 -0400

    copy input files to traj directories

--- pyctscripts/single_traj_input_setup.py
+++ pyctscripts/single_traj_input_setup.py
@@ -3,13 +3,18 @@
 # File to setup input files to perform single trajectory simulations
 
 from pathlib import Path
+from shutil import copy
 
 
 start_traj = 1
 end_traj = 100
+copy_file_names = ["Run.py", "MSD.py", "slurmscript", "simulation_parameters.yml"]
 cwd = Path.cwd()
 
 for traj_number in range(start_traj, end_traj+1):
     traj_dir_path = cwd / f'{traj_number:03}'
     Path.mkdir(traj_dir_path, parents=True, exist_ok=True)
+
+    for file_name in copy_file_names:
+        copy(cwd / file_name, traj_dir_path)
 
