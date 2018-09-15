f17ae0a4c98c29e758a7df6c67d92f2a1443bfe7
Viswanath Pasumarthi <pasumart@buffalo.edu>
2018-09-15 17:40:44 -0400

    modify the work_dir_depth value

--- pyctscripts/single_traj_input_setup.py
+++ pyctscripts/single_traj_input_setup.py
@@ -9,7 +9,9 @@ from shutil import copy, move
 start_traj = 1
 end_traj = 100
 copy_file_names = ["Run.py", "MSD.py", "slurmscript", "simulation_parameters.yml"]
-search_term = "random_seed:"
+random_seed_search_term = "random_seed:"
+work_dir_depth_search_term = "work_dir_depth:"
+work_dir_depth_addon = 2
 cwd = Path.cwd()
 
 for traj_number in range(start_traj, end_traj+1):
@@ -26,8 +28,12 @@ for traj_number in range(start_traj, end_traj+1):
     new_params_file_path = traj_dir_path / "simulation_parameters.yml.new"
     with open(old_params_file_path) as old_params_file, open(new_params_file_path, 'w') as new_params_file:
         for line in old_params_file:
-            if search_term in line:
-                new_params_file.write(f'{search_term} {traj_number}\n')
+            if random_seed_search_term in line:
+                new_params_file.write(f'{random_seed_search_term} {traj_number}\n')
+            elif work_dir_depth_search_term in line:
+                old_depth = int(line.strip().split()[1])
+                new_depth = old_depth + work_dir_depth_addon
+                new_params_file.write(f'{work_dir_depth_search_term} {new_depth}\n')
             else:
                 new_params_file.write(line)
     move(new_params_file_path, old_params_file_path)
