f74b33eafb6ba345b94905107018bc3440436fe2
'Viswanath <'pasumart@buffalo.edu'>
2018-10-01 19:09:10 -0400

    define required parameters for navigation

--- pyctscripts/submit_slurm_scripts.sh
+++ pyctscripts/submit_slurm_scripts.sh
@@ -3,6 +3,13 @@
 n_traj=100
 sleep_time=2
 
+doping_tag="undoped"
+start_carriers=4
+end_carriers=9
+t_final="1.00E-02"
+time_interval="1.00E-08"
+n_traj_key="1.00E+02"
+
 for ((i=1; i<=$n_traj; i++))
 do
     cd "traj"$i
