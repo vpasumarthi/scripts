bc13646aacf3e53f291e12c62057774a3e7397cc
'Viswanath <'pasumart@buffalo.edu'>
2018-10-01 19:27:00 -0400

    execution of pre_prod optional

--- pyctscripts/submit_slurm_scripts.sh
+++ pyctscripts/submit_slurm_scripts.sh
@@ -9,11 +9,15 @@ end_carriers=9
 t_final="1.00E-02"
 time_interval="1.00E-08"
 n_traj_key="1.00E+02"
+execute_pre_prod=1
 
 for ((j=$start_carriers; j<=$end_carriers; j++))
 do
 	cd "SimulationFiles/"$j"electrons,0holes/"$t_final"SEC,"$time_interval"TimeInterval,"$n_traj_key"Traj/no_field_"$doping_tag
-	./pre_prod.py
+	if [ $execute_pre_prod -eq 1 ]
+	then
+		./pre_prod.py
+	fi
 	for ((i=1; i<=$n_traj; i++))
 	do
 	    cd "traj"$i
