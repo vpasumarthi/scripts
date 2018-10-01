059967015120fdbf77c64d04da12199d30a16799
'Viswanath <'pasumart@buffalo.edu'>
2018-10-01 15:44:57 -0400

    define traj_dir_path variable

--- pyctscripts/generate_symlink.py
+++ pyctscripts/generate_symlink.py
@@ -13,15 +13,16 @@ def generate_symlink(src_path):
     sym_link_file_names = ['Run.py', 'simulation_parameters.yml']
     if compute_mode == 'parallel':
         for traj_index in range(n_traj):
+            traj_dir_path = src_path / f'traj{traj_index+1}'
             for file_name in sym_link_file_names:
                 src_file_path = src_path / file_name
-                dst_file_path = src_path / f'traj{traj_index+1}' / file_name
+                dst_file_path = traj_dir_path / file_name
                 os.symlink(src_file_path, dst_file_path)
 
             # generate slurm files
             slurm_file_name = 'slurmscript'
             old_slurm_file_path = src_path / slurm_file_name
-            new_slurm_file_path = src_path / f'traj{traj_index+1}' / slurm_file_name
+            new_slurm_file_path = traj_dir_path / slurm_file_name
             with open(old_slurm_file_path) as old_slurm_file, open(new_slurm_file_path, 'w') as new_slurm_file:
                 for line in old_slurm_file:
                     if slurm_search_term in line:
