ba36fed50ae5de7304728d591108b5f22eef7e82
'Viswanath <'pasumart@buffalo.edu'>
2018-10-01 19:15:20 -0400

    submit a bunch of simulations based on the defined parameters

--- pyctscripts/submit_slurm_scripts.sh
+++ pyctscripts/submit_slurm_scripts.sh
@@ -10,13 +10,18 @@ t_final="1.00E-02"
 time_interval="1.00E-08"
 n_traj_key="1.00E+02"
 
-for ((i=1; i<=$n_traj; i++))
+for ((j=$start_carriers; j<=$end_carriers; j++))
 do
-    cd "traj"$i
-    sbatch slurmscript | awk -vORS=, '{ print $4 >> "../job_ids.txt" }'
-    sleep $sleep_time
-    cd ..
-done
+	cd "SimulationFiles/"$j"electrons,0holes/"$t_final"SEC,"$time_interval"TimeInterval,"$n_traj_key"Traj/no_field_"$doping_tag
+	for ((i=1; i<=$n_traj; i++))
+	do
+	    cd "traj"$i
+	    sbatch slurmscript | awk -vORS=, '{ print $4 >> "../job_ids.txt" }'
+	    sleep $sleep_time
+	    cd ..
+	done
 
-./generate_slurm_msd.py
-sbatch slurmscript_msd
+	./generate_slurm_msd.py
+	sbatch slurmscript_msd
+	cd ../../../../
+done
