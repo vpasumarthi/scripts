19905e0bacd1e3972e2333aa7a421c3e431cafd3
'Viswanath <'pasumart@buffalo.edu'>
2018-10-01 15:04:45 -0400

    read 'n_traj' value from params file

--- pyctscripts/pre_prod.py
+++ pyctscripts/pre_prod.py
@@ -12,9 +12,10 @@ with open(cwd / 'simulation_parameters.yml') as params_file:
     for line in params_file:
         if 'compute_mode' in line:
             compute_mode = line.strip().split()[-1]
+        elif 'n_traj' in line:
+            n_traj = int(float(line.strip().split()[-1]))
 
 sym_link_file_names = ['Run.py', 'simulation_parameters.yml']
-n_traj = 100
 if compute_mode == 'parallel':
     for traj_index in range(n_traj):
         for file_name in sym_link_file_names:
