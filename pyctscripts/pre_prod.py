164b42e50467b923a1c6c4cc34a9f8950be97fe2
'Viswanath <'pasumart@buffalo.edu'>
2018-10-01 15:18:47 -0400

    moved the code into another script 'generate_symlink'

--- pyctscripts/pre_prod.py
+++ pyctscripts/pre_prod.py
@@ -1,24 +1,10 @@
 #!/usr/bin/env python
 
 from pathlib import Path
-import os
 
 from PyCT.material_preprod import material_preprod
+from pyctscripts.generate_symlink import generate_symlink
 
 cwd = Path.cwd()
 material_preprod(cwd)
-
-with open(cwd / 'simulation_parameters.yml') as params_file:
-    for line in params_file:
-        if 'compute_mode' in line:
-            compute_mode = line.strip().split()[-1]
-        elif 'n_traj' in line:
-            n_traj = int(float(line.strip().split()[-1]))
-
-sym_link_file_names = ['Run.py', 'simulation_parameters.yml']
-if compute_mode == 'parallel':
-    for traj_index in range(n_traj):
-        for file_name in sym_link_file_names:
-            src_file_path = cwd / file_name
-            dst_file_path = cwd / f'traj{traj_index+1}' / file_name
-            os.symlink(src_file_path, dst_file_path)
+generate_symlink(cwd)
