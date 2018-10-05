d426486286edffcf71b6c987cb9b862839c6b26d
'Viswanath <'pasumart@buffalo.edu'>
2018-10-05 17:09:08 -0400

    define function 'get_unit_cell_indices'

--- pyctscripts/layer_wise_residence.py
+++ pyctscripts/layer_wise_residence.py
@@ -17,8 +17,30 @@ def read_occupancy(src_path, n_traj):
         occupancy_data[traj_index+1] = np.load(traj_dir_path / occupancy_file_name)
     return occupancy_data
 
-def compute_layer_wise_residence(src_path, system_size, n_traj, gradient_ld,
-                                 layer_length_ratio):
+def get_unit_cell_indices(system_size, total_elements_per_unit_cell, n_traj,
+                          occupancy_data):
+    """Returns the unit cell indices of the element
+    :param system_size:
+    :param total_elements_per_unit_cell:
+    :param system_element_index:
+    :return:
+    """
+    unit_cell_index_data = {}
+    for traj_index in range(n_traj):
+        num_states = len(occupancy_data[traj_index+1])
+        traj_occupancy_data = occupancy_data[traj_index+1].reshape(num_states)
+        traj_unit_cell_indices = np.zeros((num_states, 3), int)
+        unit_cell_element_indices = traj_occupancy_data % total_elements_per_unit_cell
+        total_filled_unit_cells = ((traj_occupancy_data - unit_cell_element_indices)
+                                   / total_elements_per_unit_cell)
+        for index in range(3):
+            traj_unit_cell_indices[:, index] = total_filled_unit_cells / system_size[index+1:].prod()
+            total_filled_unit_cells -= traj_unit_cell_indices[:, index] * system_size[index+1:].prod()
+        unit_cell_index_data[traj_index+1] = traj_unit_cell_indices
+    return unit_cell_index_data
+
+def compute_layer_wise_residence(src_path, system_size, total_elements_per_unit_cell,
+                                 n_traj, gradient_ld, layer_length_ratio):
     """Returns the layer wise residence of charge carriers
     :param src_path:
     :param system_size:
@@ -28,4 +50,6 @@ def compute_layer_wise_residence(src_path, system_size, n_traj, gradient_ld,
     :return:
     """
     occupancy_data = read_occupancy(src_path, n_traj)
+    unit_cell_index_data = get_unit_cell_indices(
+            system_size, total_elements_per_unit_cell, n_traj, occupancy_data)
     return None
