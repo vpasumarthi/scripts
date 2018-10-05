933116f63943959e0deacbf4d76c7e6f1c37b1e9
'Viswanath <'pasumart@buffalo.edu'>
2018-10-05 16:36:39 -0400

    add 'read_occupancy' function

--- pyctscripts/layer_wise_residence.py
+++ pyctscripts/layer_wise_residence.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python
 
+import numpy as np
+
+
+def read_occupancy(src_path, n_traj):
+    """Reads the occupancy data from traj-level directories and return a 
+       dictionary of trajectory-wise occupancy data, each as a numpy array
+    :param src_path:
+    :param n_traj:
+    :return: occupancy_data:
+    """
+    occupancy_file_name = 'occupancy.npy'
+    occupancy_data = {}
+    for traj_index in range(n_traj):
+        traj_dir_path = src_path / f'traj{traj_index+1}'
+        occupancy_data[traj_index+1] = np.load(traj_dir_path / occupancy_file_name)
+    return occupancy_data
 
 def compute_layer_wise_residence(src_path, system_size, n_traj, gradient_ld,
-                         layer_length_ratio):
+                                 layer_length_ratio):
     """Returns the layer wise residence of charge carriers
     :param src_path:
     :param system_size:
@@ -12,5 +28,4 @@ def compute_layer_wise_residence(src_path, system_size, n_traj, gradient_ld,
     :return:
     """
     occupancy_data = read_occupancy(src_path, n_traj)
-    
     return None
