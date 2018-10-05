7f9ba071543b9fe98f0b4e13fd244e9acce4bb54
'Viswanath <'pasumart@buffalo.edu'>
2018-10-05 17:57:13 -0400

    use more appropriate term 'segment' instead of 'partition/layer'

--- pyctscripts/segment_wise_residence.py
+++ pyctscripts/segment_wise_residence.py
@@ -40,40 +40,40 @@ def get_unit_cell_indices(system_size, total_elements_per_unit_cell, n_traj,
         unit_cell_index_data[traj_index+1] = traj_unit_cell_indices
     return unit_cell_index_data
 
-def compute_layer_wise_residence(src_path, system_size, total_elements_per_unit_cell,
+def compute_segment_wise_residence(src_path, system_size, total_elements_per_unit_cell,
                                  n_traj, gradient_ld, partition_length_ratio):
-    """Returns the layer wise residence of charge carriers
+    """Returns the segment wise residence of charge carriers
     :param src_path:
     :param system_size:
     :param n_traj:
     :param gradient_ld:
-    :param layer_length_ratio:
+    :param segment_length_ratio:
     :return:
     """
     occupancy_data = read_occupancy(src_path, n_traj)
     unit_cell_index_data = get_unit_cell_indices(
             system_size, total_elements_per_unit_cell, n_traj, occupancy_data)
     num_elemental_partitions = np.sum(partition_length_ratio)
-    num_partitions = len(partition_length_ratio)
+    num_segments = len(partition_length_ratio)
     bin_edges = [0]
-    layer_wise_residence = np.zeros((n_traj, num_partitions), int)
-    for partition_index in range(num_partitions):
+    segment_wise_residence = np.zeros((n_traj, num_segments), int)
+    for partition_index in range(num_segments):
         bin_edges.append(bin_edges[-1] + system_size[gradient_ld] // num_elemental_partitions * partition_length_ratio[partition_index])
     for traj_index in range(n_traj):
-        layer_wise_residence[traj_index] = np.histogram(unit_cell_index_data[traj_index+1][:, 0], bin_edges)[0]
-    mean_layer_wise_residence = np.mean(layer_wise_residence, axis=0)
-    std_layer_wise_residence = np.std(layer_wise_residence, axis=0)
+        segment_wise_residence[traj_index] = np.histogram(unit_cell_index_data[traj_index+1][:, 0], bin_edges)[0]
+    mean_segment_wise_residence = np.mean(segment_wise_residence, axis=0)
+    std_segment_wise_residence = np.std(segment_wise_residence, axis=0)
     
     plt.switch_backend('Agg')
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.bar(range(1, num_partitions+1), mean_layer_wise_residence, width=0.8,
+    ax.bar(range(1, num_segments+1), mean_segment_wise_residence, width=0.8,
            color='#0504aa', alpha=0.7)
-    ax.errorbar(range(1, num_partitions+1), mean_layer_wise_residence,
-                yerr=std_layer_wise_residence, fmt='ko', capsize=3, mfc='none',
+    ax.errorbar(range(1, num_segments+1), mean_segment_wise_residence,
+                yerr=std_segment_wise_residence, fmt='ko', capsize=3, mfc='none',
                 mec='none')
-    ax.set_title('Partition-wise Mean Electron Residence')
-    ax.set_xlabel('Partition Index')
+    ax.set_title('Segment-wise Mean Electron Residence')
+    ax.set_xlabel('Segment Index')
     ax.set_ylabel('Frequency')
-    plt.savefig(str(src_path / 'layer_wise_residence.png'))
+    plt.savefig(str(src_path / 'Segment-wise Residence.png'))
     return None
