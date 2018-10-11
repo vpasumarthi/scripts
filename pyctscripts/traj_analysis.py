2c5f8f5ec754ffe8ecd241e76a63144bed66d860
'Viswanath <'pasumart@buffalo.edu'>
2018-10-11 12:04:11 -0400

    use plot_style as an input argument

--- pyctscripts/traj_analysis.py
+++ pyctscripts/traj_analysis.py
@@ -28,7 +28,7 @@ def generate_report(counts_hops, hop_proc_indices, rattle_event_array,
     return (uni_escape_dist, escape_proc_indices, escape_counts)
 
 def plot_process_analysis(disp_array_prec, max_hop_dist, bar_color, annotate,
-                          dst_path):
+                          dst_path, plot_style):
     # analysis on choice among available processes
     [unique_hop_dist, counts_hops] = np.unique(disp_array_prec,
                                                return_counts=True)
@@ -37,7 +37,7 @@ def plot_process_analysis(disp_array_prec, max_hop_dist, bar_color, annotate,
     ax = fig.add_subplot(111)
     hop_proc_indices = np.where((0 < unique_hop_dist) & (unique_hop_dist <= max_hop_dist))[0]
     xtick_items = ['%1.4f' % item for item in unique_hop_dist[hop_proc_indices]]
-    plt.bar(hop_proc_indices, counts_hops[hop_proc_indices], align='center', alpha=0.5,
+    plt.bar(hop_proc_indices, counts_hops[hop_proc_indices], align='center', alpha=1,
             edgecolor='black', color=bar_color)
     plt.xticks(hop_proc_indices, xtick_items, rotation='vertical')
 
@@ -48,9 +48,9 @@ def plot_process_analysis(disp_array_prec, max_hop_dist, bar_color, annotate,
 
     ax.set_xlabel('Hopping Distance')
     ax.set_ylabel('Frequency')
-    ax.set_yscale('log')
+    ax.set_yscale(plot_style)
     ax.set_title('Histogram of processes')
-    filename = 'process_histogram'
+    filename = f'process_histogram_{plot_style}'
     figure_name = filename + '.png'
     figure_path = dst_path / figure_name
     plt.tight_layout()
@@ -58,13 +58,14 @@ def plot_process_analysis(disp_array_prec, max_hop_dist, bar_color, annotate,
     return (counts_hops, hop_proc_indices)
 
 def plot_escape_dist_analysis(uni_escape_dist, escape_proc_indices,
-                              escape_counts, bar_color, annotate, dst_path):
+                              escape_counts, bar_color, annotate, dst_path,
+                              plot_style):
     # analysis on escape distances
     fig = plt.figure()
     ax = fig.add_subplot(111)
     escape_dist_indices = np.arange(len(uni_escape_dist[escape_proc_indices]))
     xtick_items = ['%1.4f' % item for item in uni_escape_dist[escape_proc_indices]]
-    plt.bar(escape_dist_indices, escape_counts[escape_proc_indices], align='center', alpha=0.5,
+    plt.bar(escape_dist_indices, escape_counts[escape_proc_indices], align='center', alpha=1,
             edgecolor='black', color=bar_color)
     plt.xticks(escape_dist_indices, xtick_items, rotation='vertical')
 
@@ -74,9 +75,9 @@ def plot_escape_dist_analysis(uni_escape_dist, escape_proc_indices,
                     fontweight='bold')
     ax.set_xlabel('Escape Distance')
     ax.set_ylabel('Frequency')
-    ax.set_yscale('log')
+    ax.set_yscale(plot_style)
     ax.set_title('Histogram of Escape Distances')
-    filename = 'escape_distance_histogram'
+    filename = f'escape_distance_histogram_{plot_style}'
     figure_name = filename + '.png'
     figure_path = dst_path / figure_name
     plt.tight_layout()
@@ -84,7 +85,7 @@ def plot_escape_dist_analysis(uni_escape_dist, escape_proc_indices,
     return None
 
 def plot_mobility_analysis(mobility_dist_array, max_hop_dist, bar_color,
-                           annotate, dst_path):
+                           annotate, dst_path, plot_style):
     # analysis on hopping distance contributing to mobility
     [unique_mobil_hop_dist, counts_mobil_hops] = np.unique(mobility_dist_array,
                                                            return_counts=True)
@@ -93,7 +94,7 @@ def plot_mobility_analysis(mobility_dist_array, max_hop_dist, bar_color,
     ax = fig.add_subplot(111)
     mobil_dist_indices = np.arange(len(unique_mobil_hop_dist[mobil_proc_indices]))
     xtick_items = ['%1.4f' % item for item in unique_mobil_hop_dist[mobil_proc_indices]]
-    plt.bar(mobil_dist_indices, counts_mobil_hops[mobil_proc_indices], align='center', alpha=0.5,
+    plt.bar(mobil_dist_indices, counts_mobil_hops[mobil_proc_indices], align='center', alpha=1,
             edgecolor='black', color=bar_color)
     plt.xticks(mobil_dist_indices, xtick_items, rotation='vertical')
 
@@ -103,16 +104,17 @@ def plot_mobility_analysis(mobility_dist_array, max_hop_dist, bar_color,
                     fontweight='bold')
     ax.set_xlabel('Hop Distance')
     ax.set_ylabel('Frequency')
-    ax.set_yscale('log')
+    ax.set_yscale(plot_style)
     ax.set_title('Histogram of Hop Distances contributing to mobility')
-    filename = 'mobil_hop_distance_histogram'
+    filename = f'mobil_hop_distance_histogram_{plot_style}'
     figure_name = filename + '.png'
     figure_path = dst_path / figure_name
     plt.tight_layout()
     plt.savefig(str(figure_path))
     return None
 
-def plot_rattle_analysis(rattle_dist_array, bar_color, annotate, dst_path):
+def plot_rattle_analysis(rattle_dist_array, bar_color, annotate, dst_path,
+                         plot_style):
     # analysis on hopping distance contributing to rattling
     [unique_rattle_hop_dist, counts_rattle_hops] = np.unique(
                                         rattle_dist_array, return_counts=True)
@@ -121,7 +123,7 @@ def plot_rattle_analysis(rattle_dist_array, bar_color, annotate, dst_path):
     ax = fig.add_subplot(111)
     rattle_dist_indices = np.arange(len(unique_rattle_hop_dist))
     xtick_items = ['%1.4f' % item for item in unique_rattle_hop_dist]
-    plt.bar(rattle_dist_indices, counts_rattle_hops, align='center', alpha=0.5,
+    plt.bar(rattle_dist_indices, counts_rattle_hops, align='center', alpha=1,
             edgecolor='black', color=bar_color)
     plt.xticks(rattle_dist_indices, xtick_items, rotation='vertical')
 
@@ -131,9 +133,9 @@ def plot_rattle_analysis(rattle_dist_array, bar_color, annotate, dst_path):
                     fontweight='bold')
     ax.set_xlabel('Hop Distance')
     ax.set_ylabel('Frequency')
-    ax.set_yscale('log')
+    ax.set_yscale(plot_style)
     ax.set_title('Histogram of Hop Distances contributing to rattling')
-    filename = 'rattle_hop_distance_histogram'
+    filename = f'rattle_hop_distance_histogram_{plot_style}'
     figure_name = filename + '.png'
     figure_path = dst_path / figure_name
     plt.tight_layout()
@@ -141,7 +143,7 @@ def plot_rattle_analysis(rattle_dist_array, bar_color, annotate, dst_path):
     return None
 
 def traj_analysis(dst_path, intra_poly_dist_list, max_hop_dist, disp_prec,
-                  annotate, bar_color):
+                  annotate, bar_color, plot_style):
     #NOTE: currently works with unwrapped_traj.dat which has positions at every
     # step written to it using 'write_every_step' branch of PyCT
     position_array = np.load(dst_path / 'traj1/unwrapped_traj.npy') / constants.ANG2BOHR
@@ -181,12 +183,15 @@ def traj_analysis(dst_path, intra_poly_dist_list, max_hop_dist, disp_prec,
 
     (counts_hops, hop_proc_indices) = plot_process_analysis(
                                                 disp_array_prec, max_hop_dist,
-                                                bar_color, annotate, dst_path)
+                                                bar_color, annotate, dst_path,
+                                                plot_style)
     (uni_escape_dist, escape_proc_indices, escape_counts) = generate_report(
                 counts_hops, hop_proc_indices, rattle_event_array, max_hop_dist)
     plot_escape_dist_analysis(uni_escape_dist, escape_proc_indices,
-                              escape_counts, bar_color, annotate, dst_path)
+                              escape_counts, bar_color, annotate, dst_path,
+                              plot_style)
     plot_mobility_analysis(mobility_dist_array, max_hop_dist, bar_color,
-                           annotate, dst_path)
-    plot_rattle_analysis(rattle_dist_array, bar_color, annotate, dst_path)
+                           annotate, dst_path, plot_style)
+    plot_rattle_analysis(rattle_dist_array, bar_color, annotate, dst_path,
+                         plot_style)
     return None
