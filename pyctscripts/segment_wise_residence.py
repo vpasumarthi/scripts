72b23b68f802979cb67f2ee2a394e6acda8dfc45
'Viswanath <'pasumart@buffalo.edu'>
2018-10-23 21:12:25 -0400

    plotting absolute probabilites made optional

--- pyctscripts/segment_wise_residence.py
+++ pyctscripts/segment_wise_residence.py
@@ -57,7 +57,9 @@ def get_unit_cell_indices(system_size, total_elements_per_unit_cell, n_traj,
 
 def compute_segment_wise_residence(src_path, system_size, total_elements_per_unit_cell,
                                    n_traj, gradient_ld, segment_length_ratio,
-                                   segmentwise_num_dopants, num_acceptor_sites_per_unit_cell):
+                                   segmentwise_num_dopants,
+                                   num_acceptor_sites_per_unit_cell,
+                                   show_absolute_probabilities):
     """Returns the segment wise residence of charge carriers
     :param src_path:
     :param system_size:
@@ -137,10 +139,11 @@ def compute_segment_wise_residence(src_path, system_size, total_elements_per_uni
     segment_index_list = range(1, num_segments+1)
     ax1.plot(segment_index_list, mean_segment_wise_relative_residence, 'o-',
              c='#0504aa', mfc='#0504aa', mec='black')
-    for index in range(num_segments):
-        ax1.plot([segment_index_list[index] - 0.1, segment_index_list[index] + 0.1],
-                 [segment_wise_probability[index], segment_wise_probability[index]],
-                 '-', c='#d62728')
+    if show_absolute_probabilities:
+        for index in range(num_segments):
+            ax1.plot([segment_index_list[index] - 0.1, segment_index_list[index] + 0.1],
+                     [segment_wise_probability[index], segment_wise_probability[index]],
+                     '-', c='#d62728')
 
     ax1.errorbar(segment_index_list, mean_segment_wise_relative_residence,
                  yerr=std_segment_wise_relative_residence, fmt='o', capsize=3,
