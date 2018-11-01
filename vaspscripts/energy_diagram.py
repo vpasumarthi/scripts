10c10db394cb2ac5a4cc601c65d458c09a6546ba
'Viswanath <'pasumart@buffalo.edu'>
2018-11-01 19:03:24 -0400

    allow support to use cubic spline to connect data points

--- vaspscripts/energy_diagram.py
+++ vaspscripts/energy_diagram.py
@@ -2,21 +2,31 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-
+from scipy.interpolate import interp1d
 
 def plot_energy_diagram(input_data_file_path, color_info, marker_info,
                         linestyle_info, marker_size, font_size, shift,
-                        num_forecast_periods):
+                        num_forecast_periods, cubic_spline):
     plt.switch_backend('Agg')
     energy_data = np.loadtxt(input_data_file_path)
     fig = plt.figure()
     ax = fig.add_subplot(111)
     num_data = len(energy_data)
     rc_data = np.linspace(0, 1, num_data)
-    ax.plot(rc_data[0:int(num_data / 2) + 1 + (shift if shift < 0 else 0)],
-            energy_data[0:int(num_data / 2) + 1 + (shift if shift < 0 else 0)],
-            c=color_info, marker=marker_info, ls=linestyle_info,
-            markersize=marker_size)
+    if cubic_spline:
+        spline_data = interp1d(rc_data, energy_data, kind='cubic')
+        x_new = np.linspace(rc_data[0], rc_data[-1], num=41, endpoint=True)
+        ax.plot(x_new, spline_data(x_new), c=color_info, ls=linestyle_info)
+        ax.plot(rc_data,
+                energy_data,
+                c=color_info, marker=marker_info, ls='None', markersize=marker_size)
+        
+    if not cubic_spline:
+        ax.plot(rc_data[0:int(num_data / 2) + 1 + (shift if shift < 0 else 0)],
+                energy_data[0:int(num_data / 2) + 1 + (shift if shift < 0 else 0)],
+                c=color_info, marker=marker_info, ls=linestyle_info,
+                markersize=marker_size)
+
     if num_forecast_periods > 0:
         poly = np.polyfit(rc_data[0:int(num_data / 2) + 1 + (shift if shift < 0 else 0)],
                           energy_data[0:int(num_data / 2) + 1 + (shift if shift < 0 else 0)], deg=2)
@@ -24,10 +34,11 @@ def plot_energy_diagram(input_data_file_path, color_info, marker_info,
         ax.plot(rc_data[0:int(num_data / 2) + 1 + (shift if shift < 0 else 0) + num_forecast_periods],
                 y_int, c=color_info, marker=marker_info, ls='--',
                 markersize=marker_size)
-    ax.plot(rc_data[int(num_data / 2) + (shift if shift > 0 else 0):num_data],
-            energy_data[int(num_data / 2) + (shift if shift > 0 else 0):num_data],
-            c=color_info, marker=marker_info, ls=linestyle_info,
-            markersize=marker_size)
+    if not cubic_spline:
+        ax.plot(rc_data[int(num_data / 2) + (shift if shift > 0 else 0):num_data],
+                energy_data[int(num_data / 2) + (shift if shift > 0 else 0):num_data],
+                c=color_info, marker=marker_info, ls=linestyle_info,
+                markersize=marker_size)
     if num_forecast_periods < 0:
         poly = np.polyfit(rc_data[int(num_data / 2) + (shift if shift > 0 else 0):num_data],
                           energy_data[int(num_data / 2) + (shift if shift > 0 else 0):num_data], deg=2)
