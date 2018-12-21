666fc10bb352c84027cb08734531d3f454b52853
'Viswanath <'pasumart@buffalo.edu'>
2018-12-21 17:10:45 -0500

    print unique occupancy, relative residence data

--- pyctscripts/species_distribution.py
+++ pyctscripts/species_distribution.py
@@ -9,4 +9,7 @@ def get_species_distribution(src_path):
     unique_occupancies = np.unique(occupancy)
     bin_edges = np.hstack((unique_occupancies[0], unique_occupancies[:-1] + np.diff(unique_occupancies) / 2, unique_occupancies[-1]))
     hist = np.histogram(occupancy[:-1], bins=bin_edges, weights=np.diff(time)[:, None])[0]
+    print(f'unique occupancies: {unique_occupancies}')
+    np.set_printoptions(precision=3)
+    print(f'relative residences: {hist / np.sum(hist)}')
     return None
