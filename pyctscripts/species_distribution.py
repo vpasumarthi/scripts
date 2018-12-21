075da1ebd99898e61f7379eb983104bc42b2d99b
'Viswanath <'pasumart@buffalo.edu'>
2018-12-21 17:05:27 -0500

    define bin edges and compute weighted histogram

--- pyctscripts/species_distribution.py
+++ pyctscripts/species_distribution.py
@@ -6,4 +6,7 @@ import numpy as np
 def get_species_distribution(src_path):
     occupancy = np.load(src_path / 'occupancy.npy')[()]
     time = np.load(src_path / 'time_data.npy')[()]
+    unique_occupancies = np.unique(occupancy)
+    bin_edges = np.hstack((unique_occupancies[0], unique_occupancies[:-1] + np.diff(unique_occupancies) / 2, unique_occupancies[-1]))
+    hist = np.histogram(occupancy[:-1], bins=bin_edges, weights=np.diff(time)[:, None])[0]
     return None
