fa748aa7f6d5ae48ee1d3da8d3f930e6aee897fb
'Viswanath <'pasumart@buffalo.edu'>
2019-01-02 16:43:30 -0500

    exclude the final occupant site when passing to 'occupancy_analysis'

--- pyctscripts/species_distribution.py
+++ pyctscripts/species_distribution.py
@@ -28,7 +28,7 @@ def get_unit_cell_indices(system_size, total_elements_per_unit_cell, n_traj,
 def occupancy_analysis(src_path, occupancy, time_data, site_indices):
     unique_occupancies = np.unique(occupancy)
     bin_edges = np.hstack((unique_occupancies[0], unique_occupancies[:-1] + np.diff(unique_occupancies) / 2, unique_occupancies[-1]))
-    hist = np.histogram(occupancy[:-1], bins=bin_edges, weights=time_data)[0]
+    hist = np.histogram(occupancy, bins=bin_edges, weights=time_data)[0]
     relative_residence_data = hist / np.sum(hist)
     num_shells = np.max(site_indices[:, 2])
     intersection_indices = np.in1d(site_indices[:, 0], unique_occupancies)
@@ -60,7 +60,7 @@ def occupancy_analysis(src_path, occupancy, time_data, site_indices):
 
 def get_species_distribution(src_path, system_size, total_elements_per_unit_cell,
                              n_traj, gradient_ld):
-    occupancy = np.load(src_path / 'occupancy.npy')[()]
+    occupancy = np.load(src_path / 'occupancy.npy')[()][:-1]
     time = np.load(src_path / 'time_data.npy')[()]
     time_data = np.diff(time)[:, None]
     site_indices = np.load(src_path / 'site_indices.npy')[()]
