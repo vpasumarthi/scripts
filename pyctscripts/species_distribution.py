c45d4406000a675674818d1c651f500c2bf8e5ee
'Viswanath <'pasumart@buffalo.edu'>
2019-01-02 16:08:23 -0500

    define 'get_unit_cell_indices'

--- pyctscripts/species_distribution.py
+++ pyctscripts/species_distribution.py
@@ -3,6 +3,28 @@
 import numpy as np
 
 
+def get_unit_cell_indices(system_size, total_elements_per_unit_cell, n_traj,
+                          occupancy_data):
+    """Returns the unit cell indices of the element
+    :param system_size:
+    :param total_elements_per_unit_cell:
+    :param system_element_index:
+    :return:
+    """
+    unit_cell_index_data = {}
+    for traj_index in range(n_traj):
+        traj_occupancy_data = occupancy_data[traj_index+1]
+        (num_states, num_species) = traj_occupancy_data.shape
+        traj_unit_cell_indices = np.zeros((num_states, num_species, 3), int)
+        unit_cell_element_indices = traj_occupancy_data % total_elements_per_unit_cell
+        total_filled_unit_cells = ((traj_occupancy_data - unit_cell_element_indices)
+                                   // total_elements_per_unit_cell)
+        for index in range(3):
+            traj_unit_cell_indices[:, :, index] = total_filled_unit_cells / system_size[index+1:].prod()
+            total_filled_unit_cells -= traj_unit_cell_indices[:, :, index] * system_size[index+1:].prod()
+        unit_cell_index_data[traj_index+1] = traj_unit_cell_indices
+    return unit_cell_index_data
+
 def occupancy_analysis(src_path, occupancy, time):
     unique_occupancies = np.unique(occupancy)
     bin_edges = np.hstack((unique_occupancies[0], unique_occupancies[:-1] + np.diff(unique_occupancies) / 2, unique_occupancies[-1]))
