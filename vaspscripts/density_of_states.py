b238d815aa403c17dd0fc3c8b7689fa492f3dbf5
Viswanath Pasumarthi <pasumart@buffalo.edu>
2019-01-27 18:21:29 -0500

    create get_element_spd_dos to return dos data for desired elements

--- vaspscripts/density_of_states.py
+++ vaspscripts/density_of_states.py
@@ -6,6 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pymatgen.io.vasp.outputs import Vasprun
 from pymatgen.electronic_structure.core import Spin, OrbitalType
+from pymatgen.core.periodic_table import Element
 
 def read_dos(src_path):
     # parsed object of vasprun.xml file
@@ -17,4 +18,12 @@ def read_dos(src_path):
     # fermi energy
     efermi = dosrun.efermi
     return (dosrun, cdos, tdos, efermi)
+
+def get_element_spd_dos(dos_data, desired_orbitals):
+    unique_elements = set([orbital.split("_")[0] for orbital in desired_orbitals])
+    element_spd_dos_data = {}
+    for element in unique_elements:
+        atomic_number = Element(element).number
+        element_spd_dos_data[element] = dos_data[1].get_element_spd_dos(atomic_number)
+    return element_spd_dos_data
 
