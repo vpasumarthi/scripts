b264f79ae78c488143873f2190b1d369a16120f9
Viswanath Pasumarthi <pasumart@buffalo.edu>
2019-01-27 17:44:40 -0500

    setup import statements

--- vaspscripts/density_of_states.py
+++ vaspscripts/density_of_states.py
@@ -2,4 +2,9 @@
 
 # module file with functions relating to read and plot density of states data
 
+import numpy as np
+import matplotlib.pyplot as plt
+from pymatgen.io.vasp.outputs import Vasprun
+from pymatgen.electronic_structure.core import Spin, OrbitalType
+
 
