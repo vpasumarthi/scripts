dc645123c0a47fd85c534d0783980ce0e2faf7e5
Viswanath Pasumarthi <pasumart@buffalo.edu>
2019-01-27 17:59:23 -0500

    parse vasprun.xml file to return key data

--- vaspscripts/density_of_states.py
+++ vaspscripts/density_of_states.py
@@ -7,4 +7,14 @@ import matplotlib.pyplot as plt
 from pymatgen.io.vasp.outputs import Vasprun
 from pymatgen.electronic_structure.core import Spin, OrbitalType
 
+def read_dos(src_path):
+    # parsed object of vasprun.xml file
+    dosrun = Vasprun(src_path / "vasprun.xml")
+    # complete dos object which incorporates the total dos and all projected dos
+    cdos = dosrun.complete_dos
+    # total dos at the end of the run
+    tdos = dosrun.tdos
+    # fermi energy
+    efermi = dosrun.efermi
+    return (dosrun, cdos, tdos, efermi)
 
