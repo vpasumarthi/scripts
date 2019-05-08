9a3b84ea409f39fd3326e3f10e5d8f415bbd4f0e
'Viswanath <'pasumart@buffalo.edu'>
2019-05-08 18:42:42 -0400

    read trajectory output data

--- pyctscripts/residence_analysis.py
+++ pyctscripts/residence_analysis.py
@@ -3,11 +3,17 @@
 import numpy as np
 
 
-def traj_shell_wise_residence(traj_index):
-    
+def traj_shell_wise_residence(src_path, traj_index):
+    # TODO: parse yaml file for output data file names
+    site_indices_data = np.load(f'{src_path}/traj{traj_index}/site_indices.npy')[()]
+    occupancy = np.load(f'{src_path}/traj{traj_index}/occupancy.npy')[()]
+    time = np.load(f'{src_path}/traj{traj_index}/time_data.npy')[()]
+    time_step_data = np.diff(time)
+
     return None
 
 def shell_wise_residence(src_path, n_traj, kBT, shell_wise_penalties):
+    # TODO: parse yaml file to reduce number of inputs
     num_shells = len(shell_wise_penalties)
     relative_residence_data = np.zeros((n_traj, num_shells))
     for traj_index in range(n_traj):
