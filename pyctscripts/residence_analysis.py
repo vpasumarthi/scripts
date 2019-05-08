5bc380dc2b44d46fd0c768c24dd87256585805fe
'Viswanath <'pasumart@buffalo.edu'>
2019-05-08 18:39:35 -0400

    setup trajectory-specific function to compute shell-wise residence

--- pyctscripts/residence_analysis.py
+++ pyctscripts/residence_analysis.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python
 
-def shell_wise_residence(src_path, n_traj, kBT, shell_wise_penalties):
+import numpy as np
+
+
+def traj_shell_wise_residence(traj_index):
     
+    return None
+
+def shell_wise_residence(src_path, n_traj, kBT, shell_wise_penalties):
+    num_shells = len(shell_wise_penalties)
+    relative_residence_data = np.zeros((n_traj, num_shells))
+    for traj_index in range(n_traj):
+        relative_residence_data[traj_index, :] = traj_shell_wise_residence(src_path, traj_index+1)
     return None
