6ab31cba51dbbc5a2140c56f923e1d5cdcdb400d
'Viswanath <'pasumart@buffalo.edu'>
2019-05-11 22:17:58 -0400

    take total_elements_per_unit_cell as input and define as class attribute

--- pyctscripts/residence_analysis.py
+++ pyctscripts/residence_analysis.py
@@ -8,7 +8,7 @@ from PyCT import constants
 
 
 class Residence(object):
-    def __init__(self, src_path, temp):
+    def __init__(self, src_path, temp, total_elements_per_unit_cell):
         self.src_path = src_path
         # Load simulation parameters
         sim_param_file_name = 'simulation_parameters.yml'
@@ -20,6 +20,7 @@ class Residence(object):
                 print(exc)
 
         self.kBT = constants.KB / constants.EV2J * temp  # kBT in eV
+        self.total_elements_per_unit_cell = total_elements_per_unit_cell
         self.species_count = self.sim_params['species_count']
         self.num_total_species = np.sum(self.species_count)
         self.system_size = np.asarray(self.sim_params['system_size'])
@@ -91,10 +92,10 @@ class Residence(object):
                 np.save(self.src_path / f'shell_sem_relative_residence_data_{self.dopant_element_type_list[map_index]}.npy', sem_relative_residence_data)
         return None
 
-    def traj_shell_wise_residence(self, traj_index, total_elements_per_unit_cell, shell_wise_pop_factors):
+    def traj_shell_wise_residence(self, traj_index, shell_wise_pop_factors):
         return None
 
-    def layer_wise_residence(self, total_elements_per_unit_cell, n_traj):
+    def layer_wise_residence(self, n_traj):
         for map_index, relative_energies in enumerate(self.relative_energies):
             if self.num_dopants[map_index]:
                 map_index_relative_energies = relative_energies[:]
@@ -102,7 +103,7 @@ class Residence(object):
                 relative_residence_data = np.zeros((n_traj, self.num_steps))
                 exact_relative_residence_data = np.zeros((n_traj, self.num_steps))
                 for traj_index in range(n_traj):
-                    (relative_residence_data[traj_index, :], exact_relative_residence_data[traj_index, :]) = self.traj_layer_wise_residence(traj_index+1, total_elements_per_unit_cell, shell_wise_pop_factors)
+                    (relative_residence_data[traj_index, :], exact_relative_residence_data[traj_index, :]) = self.traj_layer_wise_residence(traj_index+1, shell_wise_pop_factors)
             
                 mean_relative_residence_data = np.mean(relative_residence_data, axis=0)
                 sem_relative_residence_data = np.std(relative_residence_data, axis=0) / np.sqrt(n_traj)
