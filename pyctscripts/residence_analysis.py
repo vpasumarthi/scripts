749fb2583c10e9d439e23aa2abd7a765522e6ddf
'Viswanath <'pasumart@buffalo.edu'>
2019-06-07 14:36:30 -0400

    setting up the assumption of identical layer_length_ratio across dopants

--- pyctscripts/residence_analysis.py
+++ pyctscripts/residence_analysis.py
@@ -172,6 +172,11 @@ class Residence(object):
         return traj_relative_residence_data
 
     def layer_wise_residence(self, n_traj, interface, return_num_accessible_sites):
+        # NOTE: Assuming identical layer_length_ratio for all existing dopant element types
+        sample_existing_map_index = (np.asarray(self.num_dopants) > 0).tolist().index(True)
+        layer_length_ratio = self.doping_params['gradient'][sample_existing_map_index]['step_length_ratio']
+        num_layers = len(layer_length_ratio)
+
         for map_index, relative_energies in enumerate(self.relative_energies):
             if self.num_dopants[map_index]:
                 map_index_relative_energies = relative_energies[:]
