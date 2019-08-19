226ebf62a847c4927de7b4baef06291e766f3b1a
Viswanath Pasumarthi <pasumart@buffalo.edu>
2019-08-19 05:49:06 -0400

    get all pairwise distances

--- vaspscripts/identify_atom_pairs.py
+++ vaspscripts/identify_atom_pairs.py
@@ -5,5 +5,6 @@ import ase.io.vasp
 
 def identify_atom_pairs(src_file_path):
     cell = ase.io.vasp.read_vasp(str(src_file_path))
+    atomic_pairwise_distances = cell.get_all_distances(mic=True)
     return None
 
