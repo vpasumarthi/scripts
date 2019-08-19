7cce5602cd48bd45e3482a927bd48d0228762e83
Viswanath Pasumarthi <pasumart@buffalo.edu>
2019-08-18 22:12:23 -0400

    convert path to string

--- vaspscripts/identify_atom_pairs.py
+++ vaspscripts/identify_atom_pairs.py
@@ -4,6 +4,6 @@ import ase.io.vasp
 
 
 def identify_atom_pairs(src_file_path):
-    cell = ase.io.vasp.read_vasp(src_file_path)
+    cell = ase.io.vasp.read_vasp(str(src_file_path))
     return None
 
