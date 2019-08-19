d0883a87af5da44d8666074f8761f081a9aab708
Viswanath Pasumarthi <pasumart@buffalo.edu>
2019-08-19 06:03:06 -0400

    convert input element type to atomic number

--- vaspscripts/identify_atom_pairs.py
+++ vaspscripts/identify_atom_pairs.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
 import ase.io.vasp
+from ase.atoms import symbols2numbers
 
 
-def identify_atom_pairs(src_file_path):
+def identify_atom_pairs(src_file_path, element_type):
     cell = ase.io.vasp.read_vasp(str(src_file_path))
     atomic_pairwise_distances = cell.get_all_distances(mic=True)
+    atomic_number = symbols2numbers(element_type)[0]
     return None
 
