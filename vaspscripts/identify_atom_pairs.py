135b435e5c26355ec5b4e1de67af3841d0c1c98e
Viswanath Pasumarthi <pasumart@buffalo.edu>
2019-08-19 06:14:07 -0400

    input desired_pairwise_distance

--- vaspscripts/identify_atom_pairs.py
+++ vaspscripts/identify_atom_pairs.py
@@ -4,7 +4,7 @@ import ase.io.vasp
 from ase.atoms import symbols2numbers
 import numpy as np
 
-def identify_atom_pairs(src_file_path, element_type):
+def identify_atom_pairs(src_file_path, element_type, desired_pairwise_distance):
     cell = ase.io.vasp.read_vasp(str(src_file_path))
     atomic_pairwise_distances = cell.get_all_distances(mic=True)
     atomic_number = symbols2numbers(element_type)[0]
@@ -12,5 +12,8 @@ def identify_atom_pairs(src_file_path, element_type):
     start_index = atomic_indices[0]
     end_index = atomic_indices[-1] + 1
     element_pairwise_distances = atomic_pairwise_distances[start_index:end_index, start_index:end_index]
+
+    rounding_digits = len(desired_pairwise_distance.split(".")[1])
+    desired_pairwise_distance = float(desired_pairwise_distance)
     return None
 
