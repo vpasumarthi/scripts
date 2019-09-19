acc8fc7e6545c2826534b05f45522dd389857578
Viswanath Pasumarthi <pasumart@buffalo.edu>
2019-09-19 17:14:11 -0400

    compile and return out-of-plane pathways

--- vaspscripts/atomic_environment.py
+++ vaspscripts/atomic_environment.py
@@ -290,4 +290,6 @@ def get_plane_analysis(src_file_path, element_type, desired_pairwise_distance):
                                               lower_plane_neighbor_distance_vectors_from_pair_atom2,
                                               lower_plane_neighbor_distances_from_pair_atom2[:, None]))
     compiled_lower_plane_pathways = np.vstack((compiled_lower_plane_pathways_pair_atom1, compiled_lower_plane_pathways_pair_atom2))
-    return (cell, compiled_in_plane_pathways, compiled_upper_plane_pathways, compiled_lower_plane_pathways)
+
+    compiled_out_of_plane_pathways = np.vstack((compiled_upper_plane_pathways, compiled_lower_plane_pathways))
+    return (cell, compiled_in_plane_pathways, compiled_out_of_plane_pathways)
